57539441ae867c39371596731e4d21c8a1f5ecf3
RektvmSlvg <thomazm.silva@gmail.com>
2024-06-06 23:07:55 -0300

    camera seguindo, teste de labirinto

---
+++ TestandoGames/Assets/Scripts/CameraCtrl.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+
+public enum CameraMode 
+{ 
+    Following,
+    Cutscene,
+}
+public class CameraCtrl : MonoBehaviour
+{
+    private CameraMode mCameraMode = CameraMode.Following;
+    private Vector3 
+        offset = new(0, 0, -10f),
+        velocity = Vector3.zero;
+    [SerializeField] Transform player;
+    [SerializeField] float smoothTime = .25f;
+
+    private void FixedUpdate()
+    {
+        switch (mCameraMode)
+        {
+            case CameraMode.Following:
+                Vector3 targetPosition = player.position + offset;
+                transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
+                break;
+            case CameraMode.Cutscene:
+                break;
+            
+            default: break;
+        }
+
+        /*if(transform.position.x >= targetPosition.x - .1f || transform.position.x <= targetPosition.x + .1f ||
+            transform.position.y >= targetPosition.y - .1f || transform.position.y <= targetPosition.y + .1f)*/
+        
+    }
+
+    public void SetCamStateFollowing()
+    {
+        mCameraMode = CameraMode.Following;
+    }
+    public void SetCamStateCutscene()
+    {
+        mCameraMode = CameraMode.Cutscene;
+    }
+}
