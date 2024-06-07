553402175e5cb7f2adb9f92cd020a2340fd41199
Miguel Kowarick Athayde <miguel.athayde15@gmail.com>
2024-06-07 11:48:49 -0300

    Adicionado efeito. sonoro na colisão

--- TestandoGames/Assets/Scripts/Coletavel.cs
+++ TestandoGames/Assets/Scripts/Coletavel.cs
@@ -4,12 +4,14 @@ using UnityEngine.Events;
 public class Coletavel : MonoBehaviour
 {
     [SerializeField] UnityEvent OnCollected;
+    [SerializeField] private AudioSource pegarItem; 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             OnCollected?.Invoke();
+            pegarItem.Play();
         }
     }
 }
