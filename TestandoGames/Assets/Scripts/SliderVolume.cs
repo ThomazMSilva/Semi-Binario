69fe10f8e0f3c21a808ed7b8c1a10e578285446a
Miguel Kowarick Athayde <miguel.athayde15@gmail.com>
2024-06-07 21:18:42 -0300

    Implementação Sprites UI

--- TestandoGames/Assets/Scripts/SliderVolume.cs
+++ TestandoGames/Assets/Scripts/SliderVolume.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 public class SliderVolume : MonoBehaviour
 {
     [SerializeField] Slider barraDeVolume; 
+    [SerializeField] private GameObject menuOpcoes; 
 
     public void MudarVolume()
 
     {
         AudioListener.volume = barraDeVolume.value; 
+        DontDestroyOnLoad(menuOpcoes); 
     }
 }
